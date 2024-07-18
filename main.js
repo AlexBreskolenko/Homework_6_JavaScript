494bafb34bc49e352c60d3cd0b4a289c7fdf754b
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-07-18 19:12:12 +0300

    completed

--- main.js
+++ main.js
@@ -1,23 +1,31 @@
-//1. Найти по id, используя getElementById, элемент с id равным "super_link" и вывести этот элемент в консоль.
+//1. Найти по id, используя getElementById, элемент с id равным "super_link" и вывести этот элемент в консоль. +
 const superLinkEll = document.getElementById("super_link");
 console.log(superLinkEll);
-//2. Внутри всех элементов на странице, которые имеют класс "card-link", поменяйте текст внутри элемента на "ссылка".
+//2. Внутри всех элементов на странице, которые имеют класс "card-link", поменяйте текст внутри элемента на "ссылка". +
 const cardLinkEll = document.querySelectorAll(".card-link");
 cardLinkEll.forEach((element) => {
   element.textContent = "ссылка";
 });
-//3. Найти все элементы на странице с классом "card-link", которые лежат в элементе с классом "card-body" и вывести полученную коллекцию в консоль.
+//3. Найти все элементы на странице с классом "card-link", которые лежат в элементе с классом "card-body" и вывести полученную коллекцию в консоль.+
 const cardBodyEll = document.querySelector(".card-body");
-console.log(cardBodyEll.getElementsByClassName("card-link"));
-//4. Найти первый попавшийся элемент на странице у которого есть атрибут data-number со значением 50 и вывести его в консоль.
-// const dataAtribut = document.getElementById("data-number = "50"");
-// console.log(dataAtribut);
-//5. Выведите содержимое тега title в консоль.
+const cardLinkAll = cardBodyEll.querySelectorAll(".card-link");
+cardLinkAll.forEach((element) => {
+  console.log(element);
+});
+//4. Найти первый попавшийся элемент на странице у которого есть атрибут data-number со значением 50 и вывести его в консоль.+
+const dataNumberEll = document.querySelector('[data-number="50"]');
+console.log(dataNumberEll);
+//5. Выведите содержимое тега title в консоль. +
 const titleTeg = document.querySelector("title");
 console.log(titleTeg);
-//6. Получите элемент с классом "card-title" и выведите его родительский узел в консоль.
+//6. Получите элемент с классом "card-title" и выведите его родительский узел в консоль.+
+const cardTitleEll = document.querySelector(".card-title");
+console.log(cardTitleEll.parentElement);
+//7. Создайте тег p, запишите внутри него текст "Привет" и добавьте созданный тег в начало элемента, который имеет класс "card".+
+const paragraphEll = document.createElement("p");
+paragraphEll.textContent = "Привет";
+const cardClass = document.querySelector(".card");
+cardClass.prepend(paragraphEll);
 
-//7. Создайте тегp`, запишите внутри него текст "Привет" и добавьте созданный тег в начало элемента, который имеет класс "card".
-
-//8. Удалите тег h6 на странице.
+//8. Удалите тег h6 на странице. +
 const titleEll = document.querySelector(".mb-2").remove();
