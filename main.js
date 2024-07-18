49ab46143811830f52ff5e9f381a065853c9cd12
Alex <89929071+AlexBreskolenko@users.noreply.github.com>
2024-07-18 13:18:08 +0300

    Task 2 completed

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 //1. Найти по id, используя getElementById, элемент с id равным "super_link" и вывести этот элемент в консоль.
-const superLink = document.getElementById("super_link");
-console.log(superLink);
+const superLinkEll = document.getElementById("super_link");
+console.log(superLinkEll);
 //2. Внутри всех элементов на странице, которые имеют класс "card-link", поменяйте текст внутри элемента на "ссылка".
-
+const cardLinkEll = document.querySelectorAll(".card-link");
+cardLinkEll.forEach((element) => {
+  element.textContent = "ссылка";
+});
 //3. Найти все элементы на странице с классом "card-link", которые лежат в элементе с классом "card-body" и вывести полученную коллекцию в консоль.
 
 //4. Найти первый попавшийся элемент на странице у которого есть атрибут data-number со значением 50 и вывести его в консоль.
